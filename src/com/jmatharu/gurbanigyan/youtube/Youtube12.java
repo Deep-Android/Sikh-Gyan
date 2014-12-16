6ec426c9ba6eefe9e44d8e40a5260aee87849f9f
Deep1407 <jagdeepmatharu88@gmail.com>
2014-12-16 13:48:26 -0500

    Activity 12

---
+++ src/com/jmatharu/gurbanigyan/youtube/Youtube12.java
@@ -0,0 +1,43 @@
+package com.jmatharu.gurbanigyan.youtube;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.jmatharu.gurbanigyan.R;
+
+public class Youtube12 extends YouTubeBaseActivity implements
+		YouTubePlayer.OnInitializedListener {
+	static private final String DEVELOPER_KEY = "AIzaSyAtqdhiy9NVwbw1rEN5DKRS5m7NK6ziO70";
+	static private final String VIDEO = "";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_youtube12);
+		YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view12);
+		youTubeView.initialize(DEVELOPER_KEY, this);
+	}
+
+	@Override
+	public void onInitializationFailure(Provider arg0,
+			YouTubeInitializationResult arg1) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "Oh no! " + arg1.toString(), Toast.LENGTH_LONG)
+				.show();
+
+	}
+
+	@Override
+	public void onInitializationSuccess(Provider arg0, YouTubePlayer arg1,
+			boolean arg2) {
+		// TODO Auto-generated method stub
+		arg1.loadVideo(VIDEO);
+	}
+
+}
