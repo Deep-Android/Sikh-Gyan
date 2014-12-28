b8c892807b8c16b4f5a5a13c64fa4c7d797cc171
JMatharu <jagdeepmatharu88@gmail.com>
2014-12-28 03:15:24 -0500

    Link Update

--- src/com/jmatharu/gurbanigyan/IndexPage.java
+++ src/com/jmatharu/gurbanigyan/IndexPage.java
@@ -3,6 +3,8 @@ package com.jmatharu.gurbanigyan;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,7 +24,7 @@ public class IndexPage extends Activity {
 				Intent activityIntent = new Intent(IndexPage.this,
 						MainActivity.class);
 				IndexPage.this.startActivity(activityIntent);
-				//finish();
+				// finish();
 			}
 		});
 
@@ -38,18 +40,28 @@ public class IndexPage extends Activity {
 				finish();
 			}
 		});
-		Button bContact = (Button) findViewById(R.id.button3);
-		bContact.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent activityIntent = new Intent(IndexPage.this,
-						ContactMe.class);
-				IndexPage.this.startActivity(activityIntent);
-				finish();
-			}
-		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.contact_me) {
+			Intent activityIntent = new Intent(IndexPage.this, ContactMe.class);
+			IndexPage.this.startActivity(activityIntent);
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
